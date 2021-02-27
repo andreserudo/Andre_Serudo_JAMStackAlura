1357c597e28545e09f5cbed6b247805efcf4f9f6
Andre Serudo <andreserudo@gmail.com>
2021-02-27 15:14:19 -0400

    estado e troca de valores no form

--- src/components/commons/FormContato/index.jsx
+++ src/components/commons/FormContato/index.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Input from '../../foundation/Input';
 import FormContatoWrapper from './styles';
 
 function FormContato({ onCloseButton }) {
+  const [infoForm, setInfoForm] = useState({
+    nome: '',
+    email: '',
+    mensagem: '',
+  });
+
+  const handleChange = (event) => {
+    const fieldName = event.target.getAttribute('name');
+
+    setInfoForm({
+      ...infoForm,
+      [fieldName]: event.target.value,
+    });
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
   return (
+
     <FormContatoWrapper>
       <button
         type="button"
@@ -18,9 +34,24 @@ function FormContato({ onCloseButton }) {
       <div>
         <h1>Envie sua mensagem</h1>
         <form onSubmit={(event) => handleSubmit(event)}>
-          <Input placeholder="Nome" />
-          <Input placeholder="Email" />
-          <textarea />
+          <Input
+            placeholder="Nome"
+            name="nome"
+            value={infoForm.nome}
+            onChange={(event) => handleChange(event)}
+          />
+          <Input
+            placeholder="Email"
+            name="email"
+            value={infoForm.email}
+            onChange={(event) => handleChange(event)}
+          />
+          <textarea
+            placeholder="Sua mensagem..."
+            name="mensagem"
+            value={infoForm.mensagem}
+            onChange={(event) => handleChange(event)}
+          />
           <button
             type="submit"
           >
--- src/components/foundation/Input/index.jsx
+++ src/components/foundation/Input/index.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import InputBase from './styles';
 
-function Input({ placeholder }) {
+function Input({
+  placeholder, name, value, onChange,
+}) {
   return (
-    <InputBase placeholder={placeholder} />
+    <InputBase
+      placeholder={placeholder}
+      name={name}
+      value={value}
+      onChange={onChange}
+    />
   );
 }
 
 Input.propTypes = {
   placeholder: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+Input.defaultProps = {
+  value: '',
+  onChange: () => {},
 };
 
 export default Input;
